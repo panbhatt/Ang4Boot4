b51242c6698a22583649639a2b69925c473a1c00
Pankaj Bhatt <panbhatt@gmail.com>
2017-08-23 16:35:35 -0400

    Changes to routing module.ts

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,24 +1,36 @@
-import	{	NgModule	}	from	'@angular/core';	import	{	CommonModule	}	from	'@angular/common';
-import	{	Routes,	RouterModule	}	from	'@angular/router';	import	{	SalesComponent	}	from	'../sales/sales.component';	import	{
-SupportComponent	}	from	'../support/support.component';
-const	routes: 	Routes	=	[
-{
-path: 	'sales',
-component: 	SalesComponent
-},
-{
-path: 	'support',
-component: 	SupportComponent
-},
-{
-path: 	'**',
-component: 	SalesComponent
-}
+import {
+    NgModule
+} from '@angular/core';
+import {
+    CommonModule
+} from '@angular/common';
+import {
+    Routes,
+    RouterModule
+} from '@angular/router';
+import {
+    SalesComponent
+} from '../sales/sales.component';
+import {
+    SupportComponent
+} from '../support/support.component';
+const routes: Routes = [{
+        path: 'sales',
+        component: SalesComponent
+    },
+    {
+        path: 'support',
+        component: SupportComponent
+    },
+    {
+        path: '**',
+        component: SalesComponent
+    }
 ];
 
 @NgModule({
-imports: 	[RouterModule.forRoot(routes)],
-exports: 	[RouterModule],
-providers: 	[]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: []
 })
-export	class	RoutingModule	{	}
+export class RoutingModule {}
