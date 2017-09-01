7475933fd4724f8485544379b8d53c89bff74db9
Pankaj Bhatt <panbhatt@gmail.com>
2017-09-01 15:41:41 -0400

    Added the reactive form creational example

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm, RequiredValidator, FormControl} from '@angular/forms';
+import { NgForm, RequiredValidator, FormControl,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -10,12 +10,33 @@ export class FormsComponent implements OnInit {
 
   @ViewChild("f") f:NgForm ;
 
-  // THis is just a way so that we can use the corressponding angular component. 
+  // THis is just a way so that we can use the corressponding angular component.
   @ViewChild("fName") firstNameText : FormControl;
+
+  // Use reactive FormBuilder class to generate the forms here. 
+
+  // Reactive Forms code.
+  private formG : FormGroup ;
 
   constructor() { }
 
   ngOnInit() {
+      this.formG = new FormGroup(
+        {
+          fNameR : new FormControl("", Validators.required),
+          lNameR : new FormControl("", Validators.required)
+        }
+      )
+
+  }
+
+  onSubmit(frmGrp : FormGroup) {
+    console.log(frmGrp.valid);
+    if(frmGrp.valid) {
+      alert(JSON.stringify(frmGrp.value));
+    } else {
+      console.log(JSON.stringify(frmGrp.value));
+    }
   }
 
 }
